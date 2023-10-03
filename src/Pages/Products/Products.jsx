d45d21ddce2e6b30c6e88635bba8db2496631172
Jason Carzano <poltskie119@gmail.com>
2023-10-03 14:01:32 +0800

    final poltskie

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,22 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { categories, cartItems, items } from "../../constant/route";
 import "./Products.css";
-import { getProducts } from "../../API";
+import { Button, Flex } from "@fluentui/react-northstar";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import TextField from "@mui/material/TextField";
+import { Input } from "@fluentui/react-northstar";
+
 import { FaMagnifyingGlass } from "react-icons/fa6";
-import { BsCart3 } from "react-icons/bs";
-import { AiFillPlusCircle, AiOutlinePrinter } from "react-icons/ai";
+
+import { AiOutlineEdit } from "react-icons/ai";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { BsPlusLg } from "react-icons/bs";
 
 const Products = () => {
   const [categorize, setCategorize] = useState(categories);
   const [active, setActive] = useState("");
+  const [openDialog, setOpenDialog] = useState(false);
+
+  useEffect(() => {
+    const filtered = items.filter((item) => item.category === "Silog Meals");
+    setCategorize(filtered);
+  }, [items]);
 
   const handleChooseCategory = (c, t) => {
     setActive(t);
     const filtered = items.filter((item) => item.category === c);
     setCategorize(filtered);
   };
-  const [hoveredCardIndex, setHoveredCardIndex] = useState(null);
+  //add dialog
+  const [openAdd, setOpenAdd] = useState(false);
+  const handleClickAdd = () => {
+    setOpenAdd(true);
+  };
+  const handleCloseAdd = () => {
+    setOpenAdd(false);
+  };
 
+  //edit dialog
+  const [openEdit, setOpenEdit] = useState(false);
+  const [editItems, setEditItems] = useState([]);
+  const handleClickEdit = (item) => {
+    setEditItems(item);
+    setOpenEdit(true);
+  };
+
+  const handleCloseEdit = () => {
+    setOpenEdit(false);
+  };
+
+  // card when hover
+  const [hoveredCardIndex, setHoveredCardIndex] = useState(null);
   const handleCardHover = (index) => {
     setHoveredCardIndex(index);
   };
@@ -28,6 +65,30 @@ const Products = () => {
     { id: 5, name: "Product 5", price: 50 },
     { id: 6, name: "Product 6", price: 60 },
   ]);
+
+  const [cartItems, setCartItems] = useState([]);
+  const handleAddToCart = (item) => {
+    const existingItem = cartItems.find(
+      (cartItem) => cartItem.name === item.name
+    );
+
+    if (existingItem) {
+      const updatedCartItems = cartItems.map((cartItem) => {
+        if (cartItem.name === item.name) {
+          return {
+            ...cartItem,
+            quantity: cartItem.quantity + 1,
+            price: cartItem.price + item.price,
+          };
+        }
+        return cartItem;
+      });
+
+      setCartItems(updatedCartItems);
+    } else {
+      setCartItems([...cartItems, { ...item, quantity: 1 }]);
+    }
+  };
 
   const handleAddProduct = () => {
     const newProduct = {
@@ -48,7 +109,7 @@ const Products = () => {
   };
 
   return (
-    <div class="flex xl:flex-row lg:flex-row flex-col w-full p-4 gap-5">
+    <div class="flex xl:flex-row lg:flex-row flex-col w-full p-4 gap-5 min-h-screen scroll-hidden">
       <div class="flex-[2.8] flex-col">
         {/* search button */}
         <div className="border-[1px] rounded-md border-gray-300 w-[50%] flex items-center p-[1px] gap-2">
@@ -71,8 +132,8 @@ const Products = () => {
                 key={index}
                 className={`${
                   active === category.title
-                    ? "text-[#539e5a] bg-[#f0faee]"
-                    : "text-gray-600"
+                    ? "text-[#539e5a] bg-[#f1eded]"
+                    : "text-gray-600 "
                 } hover:bg-[#ebebeb] cursor-pointer border-[.5px] border-gray-100 bg-white shadow-md p-2 rounded-full flex gap-2 items-center`}
                 // className="hover:bg-[#ebebeb] cursor-pointer border-[.5px] border-gray-100 bg-white shadow-md p-2 rounded-full flex gap-2 items-center"
               >
@@ -83,28 +144,33 @@ const Products = () => {
         </div>
 
         {/* menu items */}
-        <div class="bg-white mt-[25px] flex flex-wrap justify-start gap-[15px]">
+        <div class="bg-white mt-[25px] flex flex-wrap justify-start gap-[20px]">
           {/* each card */}
           {categorize.map((item, index) => (
             <div
               key={index}
-              className="group relative flex flex-col p-2 hover:bg-[#f1f1f1] max-w-[170px] cursor-pointer border-gray-100 "
+              className="group shadow-md overflow-hidden relative flex flex-col p-2 hover:bg-[#f1f1f1] max-w-[190px] cursor-pointer border-gray-100 "
               onMouseEnter={() => handleCardHover(index)}
               onMouseLeave={() => handleCardHover(null)}
             >
-              {hoveredCardIndex === index && (
-                <div className="z-[2] absolute top-[30%] left-[37%] bg-transparent flex justify-center items-center">
-                  <AiFillPlusCircle
-                    className="text-5xl text-[#64cc50] shadow-xl"
-                    title="Add to cart"
-                  />
-                </div>
-              )}
               <img
                 src={item.img}
                 alt={item.img}
-                className="group-hover:brightness-50 z-[1] w-full h-[170px] group-hover:scale-105 duration-150 ease-in-out"
+                className="group-hover:brightness-50 z-[1] w-full h-[170px] group-hover:scale-105 transition-transform duration-300"
               />
+              {hoveredCardIndex === index && (
+                <div className="z-[2] absolute top-[4px] right-[0] bg-[#ebebeb] opacity-90 rounded-md flex justify-center items-center h-[30px] w-[60px] gap-[10px]">
+                  <AiOutlineEdit
+                    title="edit"
+                    className="text-[20px] hover:text-green-900"
+                    onClick={() => handleClickEdit(item)}
+                  />
+                  <RiDeleteBin6Line
+                    title="delete"
+                    className="text-[20px] hover:text-green-900"
+                  />
+                </div>
+              )}
               <p className="mt-[10px] font-bold group-hover:text-[#539e5a]">
                 {item.name}
               </p>
@@ -114,80 +180,125 @@ const Products = () => {
               </div>
             </div>
           ))}
-        </div>
-      </div>
-      {/* right section */}
-      <div class="flex-[1.2] rounded-md bg-[#f1f1f1] shadow-md border-[1px] border-gray-100 relative h-[90%]">
-        <div className="flex m-3 items-center gap-2">
-          <BsCart3 />
-          <h1>Fill your Cart now</h1>
-        </div>
-        <hr className="h-[1px] bg-gray-200 w-full"></hr>
-
-        {/* cards */}
-        {cartItems.map((item, index) => (
-          <div
-            key={index}
-            className="group flex justify-between m-4 rounded-lg"
-          >
-            {/* \left */}
-            <img
-              src={item.img}
-              alt=""
-              className="h-[60px] w-[60px] rounded-lg"
+          <div className="group h-[250px] shadow-md group hover:shadow-inner relative flex flex-col p-2 hover:bg-[#f1f1f1] w-[190px] cursor-pointer border-gray-100 bg-white">
+            <BsPlusLg
+              title="Add Bayanihan"
+              onClick={handleClickAdd}
+              className="group-hover:bg-[#ffffff] group-hover:shadow-xl rounded-full text-4xl text-[#3a772e] bg-transparent  absolute left-[40%] top-[40%]"
             />
-
-            {/* right */}
-            <div className="flex-col mt-2 justify-end">
-              <p>{item.name}</p>
-              <p>Quantity</p>
-            </div>
-            <div className="flex-col mt-2 justify-end">
-              <p>{item.price}</p>
-              <p className="text-center">{item.quantity}</p>
-            </div>
           </div>
-        ))}
-
-        {/* <hr className="h-[1px] bg-gray-200 w-full"></hr> */}
-        <div className="w-full">
-          <table className="bg-gray-200 w-full">
-            <tr>
-              <th>Product</th>
-              <th>Quantity</th>
-              <th> Price</th>
-            </tr>
-            <tr>
-              <td className="text-center">Tosilog</td>
-              <td className="text-center">2</td>
-              <td className="text-center">$360.00</td>
-            </tr>
-            <tr>
-              <td className="text-center">Tosilog</td>
-              <td className="text-center">2</td>
-              <td className="text-center">$170.00</td>
-            </tr>
-            <tr>
-              <td className="text-center">Tosilog</td>
-              <td className="text-center">2</td>
-              <td className="text-center">$360.00</td>
-            </tr>
-            <tr></tr>
-            <tr>
-              <td className="text-center"></td>
-              <td className="text-center font-bold">Total</td>
-              <td className="text-center">$490.00</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td className="flex items-center">
-                <AiOutlinePrinter />
-                Print Bill
-              </td>
-              <td></td>
-            </tr>
-          </table>
         </div>
+        {/*Add  dialog */}
+        <Dialog open={openAdd} onClose={handleCloseAdd} maxWidth="sm">
+          <DialogTitle>Add Menu</DialogTitle>
+          <DialogContent className=" p-[20px] mb-2">
+            <div className="flex p-[20px] gap-3 ">
+              <div className="flex flex-col gap-[10px]">
+                <label>Name</label>
+                <label>Category</label>
+                <label>Price</label>
+                <label>Quantity</label>
+                <label>Image</label>
+              </div>
+              <div className="flex flex-col gap-2">
+                <input
+                  type="text"
+                  name="name"
+                  className="border border-[#818181] rounded-sm outline-none px-1"
+                />
+                <select
+                  name="category"
+                  id=""
+                  className="border border-[#818181] rounded-sm outline-none "
+                >
+                  <option value="Silog Meals">Silog Meals</option>
+                  <option value="Lugaw & Mami">Lugaw & Mami</option>
+                  <option value="Sinabaw & Others">Sinabaw & Others</option>
+                  <option value="AdOns & Beverages">AdOns & Beverages</option>
+                  <option value="Bialo & Bilao">Bilao & Bilao Combo</option>
+                </select>
+                <input
+                  type="number"
+                  className="border border-[#818181] rounded-sm outline-none px-1"
+                />
+                <input
+                  type="number"
+                  className="border border-[#818181] rounded-sm outline-none px-1"
+                />
+                <input type="file" />
+              </div>
+            </div>
+            <div className="flex justify-end gap-2 mt-4 mx-[20px]">
+              <button
+                onClick={handleCloseAdd}
+                className="px-2 border hover:scale-105 duration-200 shadow-sm rounded-md bg-[#f1f1f1]"
+              >
+                Cancel
+              </button>
+              <button className="px-2 border shadow-md hover:scale-105 duration-200 rounded-md bg-[#436cc5] text-white">
+                Save
+              </button>
+            </div>
+          </DialogContent>
+        </Dialog>
+
+        {/*edit  dialog */}
+        <Dialog open={openEdit} onClose={handleCloseEdit} maxWidth="sm">
+          <DialogTitle>Edit Menu</DialogTitle>
+          <DialogContent className=" p-[20px] mb-2">
+            <div className="flex p-[20px] gap-3 ">
+              <div className="flex flex-col gap-[10px]">
+                <label>Name</label>
+                <label>Category</label>
+                <label>Price</label>
+                <label>Quantity</label>
+                <label>Image</label>
+              </div>
+              <div className="flex flex-col gap-2">
+                <input
+                  type="text"
+                  name="name"
+                  defaultValue={editItems.name}
+                  className="border border-[#818181] rounded-sm outline-none px-1"
+                />
+                <select
+                  name="category"
+                  id=""
+                  defaultValue={editItems.category}
+                  className="border border-[#818181] rounded-sm outline-none "
+                >
+                  <option value="Silog Meals">Silog Meals</option>
+                  <option value="Lugaw & Mami">Lugaw & Mami</option>
+                  <option value="Sinabaw & Others">Sinabaw & Others</option>
+                  <option value="AdOns & Beverages">AdOns & Beverages</option>
+                  <option value="Bialo & Bilao">Bilao & Bilao Combo</option>
+                </select>
+                <input
+                  type="number"
+                  defaultValue={editItems.price}
+                  className="border border-[#818181] rounded-sm outline-none px-1"
+                />
+                <input
+                  type="number"
+                  defaultValue={editItems.quantity}
+                  className="border border-[#818181] rounded-sm outline-none px-1"
+                />
+                <input type="file" />
+              </div>
+            </div>
+            <div className="flex justify-end gap-2 mt-4 mx-[20px]">
+              <button
+                onClick={handleCloseEdit}
+                className="px-2 border hover:scale-105 duration-200 shadow-sm rounded-md bg-[#f1f1f1]"
+              >
+                Cancel
+              </button>
+              <button className="px-2 border shadow-md hover:scale-105 duration-200 rounded-md bg-[#436cc5] text-white">
+                Save
+              </button>
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
